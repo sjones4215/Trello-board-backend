a4ffc50bd842a1e15a33d1aa998897d51e7fce8e
Shawn Jones <bluesfansince2013@gmail.com>
2020-09-25 22:33:45 -0500

    migration added for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_032711) do
+ActiveRecord::Schema.define(version: 2020_09_26_033210) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
@@ -30,5 +30,14 @@ ActiveRecord::Schema.define(version: 2020_09_26_032711) do
     t.index ["board_id"], name: "index_cards_on_board_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "body"
+    t.integer "card_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["card_id"], name: "index_posts_on_card_id"
+  end
+
   add_foreign_key "cards", "boards"
+  add_foreign_key "posts", "cards"
 end
